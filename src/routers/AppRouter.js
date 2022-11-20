b7e2c170a2fd1380c13f72c603cb271c2ee2a518
Vanessa Muratore <m.vanessa.dev@gmail.com>
2022-11-20 17:23:02 +0000

    Dependencies and server fixes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import DashboardPage from '../components/DashboardPage'
 import AddExpensePage from '../components/AddExpensePage'
 import EditExpensePage from '../components/EditExpensePage'
-import HelpPage from '../components/HelpPage'
 import NotFoundPage from '../components/NotFoundPage'
 import Header from '../components/Header'
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,9 @@ module.exports = (env) => {
 	return {
 		entry: './src/app.js',
 		output: {
-			path: path.join(__dirname, 'public'),
-			filename: 'bundle.js'
+			path: path.join(__dirname, 'public', 'dist'),
+			filename: 'bundle.js',
+			publicPath: '/dist/'
 		},
 		plugins: [new MiniCssExtractPlugin()],
 		module: {
@@ -40,6 +41,7 @@ module.exports = (env) => {
 				directory: path.resolve(__dirname, 'public')
 			},
 			historyApiFallback: true
+			// publicPath: '/dist/'
 		}
 	}
 }
